c85c1ebeb05f2936ed141baa9ae36d9ee6eb7542
Athanasios Oikonomou <athoik@gmail.com>
2014-12-11 19:09:12 +0200

    OMBManager: check if getImageFileSystem contains jffs2
    Required for dreamboxes because filesystem is jffs2.nfi
    
    >>> getImageFileSystem()
    'jffs2.nfi'

--- src/OMBManager.py
+++ src/OMBManager.py
@@ -171,15 +171,15 @@ class OMBManagerKernelModule:
 		
 def OMBManager(session, **kwargs):
 	found = False
-	
+
 	kernel_module = 'kernel-module-nandsim'
-	if OMB_GETIMAGEFILESYSTEM == "jffs2":
+	if "jffs2" in OMB_GETIMAGEFILESYSTEM:
 		kernel_module = 'kernel-module-block2mtd'
 	
 	if os.system('opkg list_installed | grep ' + kernel_module) != 0 and BRANDING:
 		OMBManagerKernelModule(session, kernel_module)
 		return
-	
+
 	data_dir = OMB_MAIN_DIR + '/' + OMB_DATA_DIR
 	if os.path.exists(data_dir):
 		session.open(OMBManagerList, OMB_MAIN_DIR)
