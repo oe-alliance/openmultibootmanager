c48624f6386fb709e336de9e6f5b6015d395c70f
a4tech <darezik@gmail.com>
2016-03-25 22:56:31 +0100

    Re-add Check if OMB_MAIN_DIR exists before image install

--- src/OMBManagerInstall.py
+++ src/OMBManagerInstall.py
@@ -29,7 +29,7 @@ from Components.Sources.List import List
 
 from Tools.Directories import fileExists
 
-from OMBManagerCommon import OMB_DATA_DIR, OMB_UPLOAD_DIR, OMB_TMP_DIR
+from OMBManagerCommon import OMB_MAIN_DIR, OMB_DATA_DIR, OMB_UPLOAD_DIR, OMB_TMP_DIR
 from OMBManagerLocale import _
 
 from enigma import eTimer
@@ -203,6 +203,13 @@ class OMBManagerInstall(Screen):
 		kernel_target_folder = self.mount_point + '/' + OMB_DATA_DIR + '/.kernels'
 		kernel_target_file = kernel_target_folder + '/' + selected_image_identifier + '.bin'
 
+		if not os.path.exists(OMB_MAIN_DIR):
+			try:
+				os.makedirs(OMB_MAIN_DIR)
+			except OSError as exception:
+				self.showError(_("Cannot create main folder %s") % OMB_MAIN_DIR)
+				return
+
 		if not os.path.exists(kernel_target_folder):
 			try:
 				os.makedirs(kernel_target_folder)
@@ -347,7 +354,7 @@ class OMBManagerInstall(Screen):
 			self.showError(_("Cannot create virtual MTD device"))
 			return False
 
-		if OMB_GETBRANDOEM in ('xcore'):
+		if not os.path.exists('/dev/mtdblock' + mtd):
 			os.system(OMB_DD_BIN + ' if=' + rootfs_path + ' of=/dev/mtd' + mtd + ' bs=2048')
 		else:
 			os.system(OMB_DD_BIN + ' if=' + rootfs_path + ' of=/dev/mtdblock' + mtd + ' bs=2048')
