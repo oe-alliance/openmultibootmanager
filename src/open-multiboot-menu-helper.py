b9bb93edcc649d2ee899a59df3bafc3b33a1334a
dpeddi <eddi@depieri.net>
2021-08-05 00:33:41 +0200

    fix looking for omb dir in menu helper

--- src/open-multiboot-menu-helper.py
+++ src/open-multiboot-menu-helper.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+from __future__ import print_function
 import sys
 
 sys.path.insert(0, '/usr/lib/enigma2/python/Plugins/Extensions/')
@@ -6,7 +8,12 @@ from OpenMultiboot import OMBList
 
 if __name__ == "__main__":
 
-	ombList = OMBList.OMBList("/media/usb", debug=None)
+	ombdir = "/omb"
+
+	if len(sys.argv) == 2:
+		ombdir = sys.argv[1]
+
+	ombList = OMBList.OMBList( ombdir, debug=None)
 
 	ombList.populateImagesList()
 	print (ombList.getJson())
