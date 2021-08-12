3928266ca9b7c10b0d3636ec4bea30ab89bb1a19
dpeddi <eddi@depieri.net>
2021-08-12 23:51:51 +0200

    add debug parameter to open-multiboot-menu-helper
    fix issue on duo2 displaymodel string

--- src/open-multiboot-menu-helper.py
+++ src/open-multiboot-menu-helper.py
@@ -8,13 +8,17 @@ from OpenMultiboot import OMBList
 
 if __name__ == "__main__":
 
+	debug = None
 	ombdir = "/omb"
 
 	if len(sys.argv) == 2:
 		ombdir = sys.argv[1]
+	
+	if len(sys.argv) == 3 and sys.argv[2].upper() == "DEBUG":
+		debug = True
 
-	ombList = OMBList.OMBList( ombdir, debug=None)
+	ombList = OMBList.OMBList(ombdir, debug = debug)
 
 	ombList.populateImagesList()
-	print (ombList.getJson())
+	print (ombList.getJson(debug = debug))
 
